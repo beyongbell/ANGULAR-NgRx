1976c5adb854199d38804abeafc1fa47a7a70603
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-09-14 21:51:47 +0700

    Create aAction Reducer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
 
+import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  {
+    path: "customers",
+    loadChildren: "../app/customers/customers.module#CustomersModule"
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [CommonModule, RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+  declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { CustomerComponent } from './customer/customer.component';
-import { CustomerAddComponent } from './customer-add/customer-add.component';
-import { CustomerEditComponent } from './customer-edit/customer-edit.component';
-import { CustomerListComponent } from './customer-list/customer-list.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { CustomerComponent } from "./customer/customer.component";
+import { CustomerAddComponent } from "./customer-add/customer-add.component";
+import { CustomerEditComponent } from "./customer-edit/customer-edit.component";
+import { CustomerListComponent } from "./customer-list/customer-list.component";
 
-
-const customerRoutes: Routes = [];
+const customerRoutes: Routes = [{ path: "", component: CustomerComponent }];
 
 @NgModule({
-  declarations: [CustomerComponent, CustomerAddComponent, CustomerEditComponent, CustomerListComponent],
-  imports: [CommonModule, RouterModule.forChild(customerRoutes)],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forChild(customerRoutes),
+  ],
+  declarations: [
+    CustomerComponent,
+    CustomerAddComponent,
+    CustomerEditComponent,
+    CustomerListComponent
+  ]
 })
-
-export class CustomersModule { }
+export class CustomersModule {}
