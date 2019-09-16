64dea6e66228bb1151d3d236fc6cb50cb0259a84
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-09-16 14:06:01 +0700

    Remove Comment

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -20,19 +20,14 @@ export interface State {
 export class CustomSerializer implements RouterStateSerializer<RouterStateUrl> {
   serialize(routerState: RouterStateSnapshot): RouterStateUrl {
     let route = routerState.root;
-
     while (route.firstChild) {
       route = route.firstChild;
     }
-
     const {
       url,
       root: { queryParams }
     } = routerState;
     const { params } = route;
-
-    // Only return an object including the URL, params and query params
-    // instead of the entire snapshot
     return { url, params, queryParams };
   }
 }
